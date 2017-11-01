d6f5d588ef6fe4620d2180d73c045169fe1bdb83
Stuart Jansen <sjansen@buscaluz.org>
2017-10-31 19:40:20 -0600

    add a simple disk usage reporting example

---
+++ spikes/du/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	humanize "github.com/dustin/go-humanize"
+	"github.com/shirou/gopsutil/disk"
+)
+
+func main() {
+	partitions, err := disk.Partitions(false)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, p := range partitions {
+		fmt.Printf(
+			"Mountpoint: %v  Fstype: %v\n",
+			p.Mountpoint, p.Fstype,
+		)
+		if u, err := disk.Usage(p.Mountpoint); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf(
+				"Blocks: %2.0f%% (%s/%s)\n",
+				u.UsedPercent,
+				humanize.IBytes(u.Used),
+				humanize.IBytes(u.Total),
+			)
+			fmt.Printf(
+				"Inodes: %2.0f%% (%d/%d)\n",
+				u.InodesUsedPercent,
+				u.InodesUsed,
+				u.InodesTotal,
+			)
+		}
+		fmt.Println("")
+	}
+}
