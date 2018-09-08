92fa2c66d5bc085511a651381602a90980c76816
Stuart Jansen <sjansen@buscaluz.org>
2018-09-08 12:26:02 -0600

    add main.go & version.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	app := kingpin.
+		New("mecha", "A tool to make software development easier").
+		UsageTemplate(kingpin.CompactUsageTemplate)
+
+	v := &versionCmd{}
+	v.register(app)
+
+	if len(os.Args) == 1 {
+		app.Usage(os.Args[1:])
+		os.Exit(1)
+	}
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+}
+
+type versionCmd struct{}
+
+func (v *versionCmd) register(app *kingpin.Application) {
+	app.Command("version", "Print mecha's version").Action(v.run)
+}
+
+func (v *versionCmd) run(pc *kingpin.ParseContext) error {
+	fmt.Println("mecha", version)
+	return nil
+}
