d5e989f8625312219e000e8bb60764da12c4a001
Stuart Jansen <sjansen@buscaluz.org>
2018-10-05 07:23:16 -0600

    update tui spike

--- spikes/tui/main.go
+++ spikes/tui/main.go
@@ -10,53 +10,88 @@ import (
 	"github.com/rivo/tview"
 )
 
-var boxes = []string{"foo", "bar", "baz", "qux", "quux", "corge"}
+var titles = []string{
+	"foo", "bar", "baz", "qux", "quux", "corge",
+	"grault", "garply", "waldo", "fred", "plugh",
+	"xyzzy", "thud",
+}
 
-func addView() *tview.TextView {
-	textView := tview.NewTextView().
+type screen struct {
+	app   *tview.Application
+	cols  *tview.Flex
+	rows  *tview.Flex
+	views []*tview.TextView
+}
+
+func (s *screen) addView() {
+	idx := len(s.views)
+	view := tview.NewTextView().
 		SetDynamicColors(true)
+	view.SetBorder(true).
+		SetTitle(titles[idx]).
+		SetTitleAlign(tview.AlignLeft)
+	view.SetChangedFunc(func() {
+		s.app.Draw()
+	})
 	go func() {
-		for i := 1; i <= 25; i++ {
+		for i := 1; i <= 15; i++ {
 			if i%10 == 0 {
-				fmt.Fprintf(textView, "[red::r]line #%d[-:-:-]\n", i)
+				fmt.Fprintf(view, "[red::r]line #%d[-:-:-]\n", i)
 			} else {
-				fmt.Fprintf(textView, "[green]line #%d[-:-:-]\n", i)
+				fmt.Fprintf(view, "[green]line #%d[-:-:-]\n", i)
 			}
-			n := rand.Int()%900 + 100
+			n := rand.Int()%750 + 250
 			time.Sleep(time.Duration(n) * time.Millisecond)
 		}
-		fmt.Fprintf(textView, "[yellow]Press Enter to quit.")
+		fmt.Fprintf(view, `[yellow]Press "q" to quit.`)
 	}()
-	return textView
+	s.rows.AddItem(view, 0, 1, false)
+	s.views = append(s.views, view)
 }
 
 func main() {
 	app := tview.NewApplication()
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		key := event.Key()
-		if key == tcell.KeyEnter {
-			app.Stop()
-		} else if key == tcell.KeyRune && event.Rune() == 'q' {
-			app.Stop()
-		}
-		return event
-	})
-
-	flex := tview.NewFlex().
+	cols := tview.NewFlex().
+		SetDirection(tview.FlexColumn)
+	menu := tview.NewList().
+		AddItem("Add Row", "", 0, nil).
+		AddItem("Quit", "", 0, nil).
+		ShowSecondaryText(false)
+	menu.SetBorderPadding(1, 1, 1, 1)
+	rows := tview.NewFlex().
 		SetDirection(tview.FlexRow)
-	for _, title := range boxes {
-		textView := addView().
-			SetChangedFunc(func() {
-				app.Draw()
-			})
-		textView.
-			SetBorder(true).
-			SetTitle(title).
-			SetTitleAlign(tview.AlignLeft)
-		flex.AddItem(textView, 0, 1, false)
+	cols.AddItem(menu, 15, 0, true)
+	cols.AddItem(rows, 0, 1, false)
+
+	screen := screen{
+		app:   app,
+		cols:  cols,
+		rows:  rows,
+		views: make([]*tview.TextView, 0, 3),
 	}
 
-	if err := app.SetRoot(flex, true).Run(); err != nil {
+	app.SetInputCapture(func(e *tcell.EventKey) *tcell.EventKey {
+		key := e.Key()
+		if key == tcell.KeyRune && e.Rune() == 'q' {
+			app.Stop()
+		}
+		return e
+	})
+
+	menu.SetSelectedFunc(func(_ int, action string, _ string, _ rune) {
+		switch action {
+		case "Add Row":
+			screen.addView()
+		case "Quit":
+			app.Stop()
+		}
+	})
+
+	for i := 0; i < cap(screen.views); i++ {
+		screen.addView()
+	}
+
+	if err := app.SetRoot(cols, true).Run(); err != nil {
 		panic(err)
 	}
 }
