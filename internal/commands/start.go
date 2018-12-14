1fef01223d0555e6374c0467132e73f2e703369a
Stuart Jansen <sjansen@buscaluz.org>
2018-12-13 19:43:42 -0700

    add real RAM status to start command

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -1,9 +1,12 @@
 package commands
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
+	humanize "github.com/dustin/go-humanize"
+	"github.com/shirou/gopsutil/mem"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/sjansen/mecha/internal/tui"
@@ -21,7 +24,7 @@ func (cmd *startCmd) run(pc *kingpin.ParseContext) error {
 	screen.AddStdView("TODO", nil, nil).
 		AddStdView("TODO", nil, nil)
 
-	for _, label := range []string{"Clock:", "Disk:", "RAM:"} {
+	for _, label := range []string{"Clock:", "Disk:"} {
 		updates := make(chan *tui.Status)
 		screen.AddStatusItem(label, updates)
 		go func() {
@@ -42,5 +45,36 @@ func (cmd *startCmd) run(pc *kingpin.ParseContext) error {
 		}()
 	}
 
+	screen.AddStatusItem("RAM:", startMemoryStatus())
+
 	return screen.Run()
+}
+
+func startMemoryStatus() chan *tui.Status {
+	updates := make(chan *tui.Status)
+	go func() {
+		for {
+			update := &tui.Status{}
+			if x, err := mem.VirtualMemory(); err != nil {
+				update.Severity = tui.Unknown
+				update.Message = "???"
+			} else {
+				if x.UsedPercent < 85 {
+					update.Severity = tui.Healthy
+				} else if x.UsedPercent < 95 {
+					update.Severity = tui.Warning
+				} else {
+					update.Severity = tui.Alert
+				}
+				update.Message = fmt.Sprintf("%2.0f%% (%s/%s)",
+					x.UsedPercent,
+					humanize.IBytes(x.Used),
+					humanize.IBytes(x.Total),
+				)
+			}
+			updates <- update
+			time.Sleep(5 * time.Second)
+		}
+	}()
+	return updates
 }
