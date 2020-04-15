294668f921820eaf3041f806561c8d9189528268
Stuart Jansen <sjansen@buscaluz.org>
2020-04-14 22:21:53 -0600

    reimplement spikes/arg0/

--- spikes/arg0/main.go
+++ spikes/arg0/main.go
@@ -3,36 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
-var arg0 struct {
-	err error
-	val string
-}
-
-func init() {
-	arg0 := os.Args[0]
-	switch {
-	case filepath.IsAbs(arg0):
-		setARG0(arg0, nil)
-	case strings.ContainsRune(arg0, os.PathSeparator):
-		setARG0(filepath.Abs(arg0))
-	default:
-		setARG0(exec.LookPath(arg0))
-	}
-}
-
-func setARG0(path string, err error) {
-	if err == nil {
-		path, err = filepath.EvalSymlinks(path)
-	}
-	arg0.err = err
-	arg0.val = path
-}
-
 func main() {
-	fmt.Printf("%q (%v)\n", arg0.val, arg0.err)
+	path, err := os.Executable()
+	fmt.Printf("%q (%v)\n", path, err)
 }
